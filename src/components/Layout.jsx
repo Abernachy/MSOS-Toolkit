c5f5390af86d4b1a35986b4b1d9c74c116e8d0df
Michael <49463165+Abernachy@users.noreply.github.com>
2022-03-22 18:22:21 +0000

    Figuring out this mobile stuff.  It's a pain in the butt, lols.  Comitting so I can make a localhost of the repo. blah blablah, I need a beer.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,26 @@
 import Nav from './Nav'
 import Footer from './Footer'
+import Head from 'next/head'
 import layoutStyle from '../../styles/Layout.module.css'
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
 
   return (
     <>
+      <Head>
+        <title>621 MSOS</title>
+        <meta name="keywords" content=" 621 MSOS , 621 Mission Support Operations Squadron, MSOS Toolkit" key="keywords" />
+        <meta name="description" content="621 MSOS Toolkit" key="description" />
+        <meta name="author" content="Michael Mason" key="author"/>
+        <meta name="viewport" content="width=device-width , initial-scale=1.0" key="viewport" />
+      </Head>
       <Nav />
+
       <div className={layoutStyle.container}>
         <main>
           {children}
         </main>
-      <Footer />
+        <Footer />
       </div>
     </>
   )
---
+++ src/utils/tagElementsData.jsx
@@ -0,0 +1,6 @@
+const tagElementsData = [
+  { title: 'Big movement coming and don\'t know what to do?',
+   advertisement: 'Our EAGLE Team services will give you the training you need to prepare for your unit\'s next big move.'
+    
+  }
+]
