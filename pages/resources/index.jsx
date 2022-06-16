6f4fdff33c3c73ab46d7c5bd4218b22872b04558
Michael Mason <mikemason1987@gmail.com>
2022-06-16 12:03:30 -0400

    redid the main resources index page

--- pages/resources/index.jsx
+++ pages/resources/index.jsx
@@ -1,36 +1,51 @@
 //next imports
 import Link from 'next/link'
 //css
-import style from './styles/resourcesMain.module.css'
+import style from './styles/globalResources.module.css'
 //component imports
 import ResourceContainer from '../../src/components/ResourceContainer/ResourceContainer'
 import Card from '../../src/components/Card/Card'
 
+const { topHeader, mainBody, lineDivider, resourcesSection, headerBlock } =
+	style
+
 const index = () => {
 	return (
-		<div className={style.resourceContainer}>
-			<section className={style.resourceHeader}>
-				<h1>RESOURCES</h1>
+		<div>
+			<section className={topHeader}>
+				<h1>621 MSOS Resources</h1>
 			</section>
-			<section className={style.mainBody}>
+			<section className={mainBody}>
 				<p>
 					Whether you are a 621 MSOS customer, an EAGLE member, an AMLO, or
-					a future member of the squadron (welcome) resources are provided
-					below at no cost to you.
+					a future member of the squadron (welcome), resources are provided
+					below for your benefit at no cost. If you feel a resource should
+					be added to any of these, please do not hesitate to reach out to
+					us.
 				</p>
-				<div className={style.lineDivider} />
-				<section className={style.resourceList}>
-					<section className={style.resources}>
-						<h3>Customer Resources</h3>
+				<div className={lineDivider} />
+			</section>
+			<section
+				className={resourcesSection}
+				style={{ textAlign: 'center' }}>
+				<div>
+					<div className={headerBlock}>
+						<h2>Customer Resources</h2>
+					</div>
+					<ResourceContainer>
 						<Card
 							name='Customer Resources'
 							path='./resources/customer-resources'
 							img='/imgs/airTranspulling.webp'
 							description='Resources for customers'
 						/>
-					</section>
-					<section className={style.resources}>
-						<h3>Squadron Member Resources</h3>
+					</ResourceContainer>
+				</div>
+				<div>
+					<div className={headerBlock}>
+						<h2>Squadron Member Resources</h2>
+					</div>
+					<ResourceContainer>
 						<Card
 							name='AMLO Resources'
 							path='./resources/amlo-resources'
@@ -43,18 +58,14 @@ const index = () => {
 							img='/imgs/lemon.jpg'
 							description=' 621 MSOS resources for EAGLEs'
 						/>
-					</section>
-					<section className={style.resources}>
-						<h3>Newcomer Resources</h3>
-
 						<Card
 							name='Newcomer Resources'
 							path='/resources/newcomer-resources'
 							img='/imgs/JBMDL-Welcome.webp'
 							description='Resources for new and upcoming squadron members'
 						/>
-					</section>
-				</section>
+					</ResourceContainer>
+				</div>
 			</section>
 		</div>
 	)
